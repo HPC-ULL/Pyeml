4b1803bb63d1cb48cdfff2e0d493c669ac9fede1
Daniel Suarez Labena <alu0101040882@ull.edu.es>
2022-11-24 16:17:25 +0000

    update

--- src/pyeml/pyeml.py
+++ src/pyeml/pyeml.py
@@ -1,7 +1,7 @@
 
 import sys
 import os
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__),"lib"))
 from eml import measureCode
 
 from inspect import currentframe
