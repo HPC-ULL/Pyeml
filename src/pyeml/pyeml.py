ef0914252be6a98c93730065ac66ee5af2e9fb1c
Daniel Suarez Labena <alu0101040882@ull.edu.es>
2023-01-23 20:29:52 +0000

    update

--- src/pyeml/pyeml.py
+++ src/pyeml/pyeml.py
@@ -17,15 +17,16 @@ if EML_LIB_PATH not in os.getenv('LD_LIBRARY_PATH',""):
     os.environ["LD_LIBRARY_PATH"] = os.getenv('LD_LIBRARY_PATH',"") + ":" + EML_LIB_PATH
     print(f"Adding '{EML_LIB_PATH}' to LD_LIBRARY_PATH")
     try:
-        exe,arg = '',''
-        if(sys.argv[0] == ''): #interpreter
-            exe = sys.executable
-            arg = "import pyeml"
-        else:
-            exe = sys.argv[0]
-            arg = sys.argv
+
+        if(sys.argv[0] != ''):
+            sys.stdout.flush()
+            os.execl(sys.executable,sys.executable, *sys.argv)
+
+        
+    except OSError as e:
+        print(e)
     except Exception as e:
-        print(f'Failed re-exec: ({exe, arg})', e)
+        print(f'Failed re-exec:', e)
         sys.exit(1)
 
 
