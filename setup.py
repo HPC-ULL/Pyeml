1a7b6cb94d47ade9c86d70e000a3a6122c3da110
Daniel Suarez Labena <alu0101040882@ull.edu.es>
2023-01-23 17:12:58 +0000

    update

--- setup.py
+++ setup.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python
 
-import sys
 
-# Available at setup time due to pyproject.toml
-from pybind11 import get_cmake_dir
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 __version__ = "0.0.1"
 
-
 setup_helper = Pybind11Extension("eml",
         ["src/pyeml/lib/eml.cpp"],
-        # Example: passing in the version to the compiled code
-        define_macros = [('VERSION_INFO', __version__)],
         )
 
 
 #, "-Wl,--as-needed",  "-lnvidia-ml" ,  "-Wl,--no-as-needed"
 
 setup_helper._add_cflags(["-shared"])
-setup_helper._add_ldflags(["-L/usr/local/lib", "-leml" ,"-lconfuse", "-std=c++11"])
+setup_helper._add_ldflags(["-L/usr/local/lib", "-leml" ,"-lconfuse", "-std=c++11", "-Wl,--as-needed",  "-lnvidia-ml" ,  "-Wl,--no-as-needed"])
 
-ext_modules = [
- setup_helper
-]
 
 setup(
     name="pyeml",
@@ -32,11 +23,10 @@ setup(
     author="Daniel Suárez Labena",
     author_email="dsuarezl@ull.edu.es",
     url="https://github.com/dsuarezl/Pyeml",
-    description="A test project using pybind11",
+    description="Tool to measure energy consumption of python code",
     long_description="",
-    ext_modules=ext_modules,
-    # Currently, build_ext only provides an optional "highest supported C++
-    # level" feature, but in the future it may provide more features.
+    ext_modules=[setup_helper],
+
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.7",
